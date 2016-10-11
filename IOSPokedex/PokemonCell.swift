0231df16ecdc238774252d68d61963e4d7958c0a
codeerish <codeerish.solutions@gmail.com>
2016-10-11 10:47:01 +0800

    Last Commit PokemonCell

---
+++ IOSPokedex/PokemonCell.swift
@@ -0,0 +1,24 @@
+//
+//  PokemonCell.swift
+//  IOSPokedex
+//
+//  Created by Erish Latorre on 10/11/16.
+//  Copyright © 2016 Pokerish. All rights reserved.
+//
+
+import UIKit
+
+class PokemonCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
